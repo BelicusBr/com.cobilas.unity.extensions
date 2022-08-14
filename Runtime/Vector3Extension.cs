7fe90fa804605b16fb994e5d912556eca67e4c08
Gabriel dos Santos Severino <93249822+BelicusBr@users.noreply.github.com>
2022-08-14 01:44:47 -0300

    Update Vector3Extension.cs

--- Runtime/Vector3Extension.cs
+++ Runtime/Vector3Extension.cs
@@ -62,50 +62,5 @@ namespace UnityEngine {
         /// <summary>Soma todos os eixos.</summary>
         public static float Summation(this Vector3 v)
             => v.x + v.y + v.z;
-        #region Old code
-        [Obsolete("Use Vector3.ABS(bool, bool, bool)")]
-        public static Vector3 ABSVector3(this Vector3 V, bool x = true, bool y = true, bool z = true)
-            => ABS(V, x, y, z);
-        [Obsolete("Use Vector3.ABSX()")]
-        public static Vector3 ABSVector3X(this Vector3 V)
-            => ABSX(V);
-        [Obsolete("Use Vector3.ABSY()")]
-        public static Vector3 ABSVector3Y(this Vector3 V)
-            => ABSY(V);
-        [Obsolete("Use Vector3.ABSZ()")]
-        public static Vector3 ABSVector3Z(this Vector3 V)
-            => ABSZ(V);
-        [Obsolete("Use Vector3.Invert(bool, bool, bool)")]
-        public static Vector3 InvertVector3(this Vector3 V, bool x = true, bool y = true, bool z = true)
-            => Invert(V, x, y, z);
-        [Obsolete("Use Vector3.InvertX()")]
-        public static Vector3 InvertVector3X(this Vector3 V)
-            => InvertX(V);
-        [Obsolete("Use Vector3.InvertY()")]
-        public static Vector3 InvertVector3Y(this Vector3 V)
-            => InvertY(V);
-        [Obsolete("Use Vector3.InvertZ()")]
-        public static Vector3 InvertVector3Z(this Vector3 V)
-            => InvertZ(V);
-        [Obsolete("Use Vector3.Division(Vector3)")]
-        public static Vector3 Divisor(this Vector3 V, Vector3 B) 
-            => new Vector3(V.x / B.x, V.y / B.y, V.z / B.z);
-        [Obsolete("Use Vector3.Multiplication(Vector3)")]
-        public static Vector3 Multiplicador(this Vector3 V, Vector3 B)
-            => new Vector3(V.x * B.x, V.y * B.y, V.z * B.z);
-        [Obsolete("Use Vector3.ToVector3Int()")]
-        public static Vector3Int ParaVector3Int(this Vector3 V)
-            => new Vector3Int((int)V.x, (int)V.y, (int)V.z);
-        [Obsolete("Use Vector3.Summation()")]
-        public static float SomaDeTodosOsEixos(this Vector3 V)
-            => V.x + V.y + V.z;
-        [Obsolete]
-        public static Vector3 RemoverElevadosAPotencia(this Vector3 V)
-            => new Vector3(
-                V.x.ToString().ToLower().Contains("e") ? 0 : V.x,
-                V.y.ToString().ToLower().Contains("e") ? 0 : V.y,
-                V.z.ToString().ToLower().Contains("e") ? 0 : V.z
-                );
-        #endregion
     }
 }
