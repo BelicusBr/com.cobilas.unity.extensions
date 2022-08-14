d08298f03dceface813ee0b05935e7cd082afd47
Gabriel dos Santos Severino <93249822+BelicusBr@users.noreply.github.com>
2022-08-14 01:42:11 -0300

    Update Vector2IntExtension.cs

--- Runtime/Vector2IntExtension.cs
+++ Runtime/Vector2IntExtension.cs
@@ -35,43 +35,5 @@ namespace UnityEngine {
         /// <summary>Soma todos os eixos.</summary>
         public static int Summation(this Vector2Int v)
             => v.x + v.y;
-
-        #region Old code
-        [Obsolete("Use Vector2Int.ABS(bool, bool)")]
-        public static Vector2Int ABSVector2Int(this Vector2Int V, bool x = true, bool y = true)
-            => ABS(V, x, y);
-        [Obsolete("Use Vector2Int.ABSX()")]
-        public static Vector2Int ABSVector2IntX(this Vector2Int V)
-            => ABSX(V);
-        [Obsolete("Use Vector2Int.ABSY()")]
-        public static Vector2Int ABSVector2IntY(this Vector2Int V)
-            => ABSY(V);
-        [Obsolete("Use Vector2Int.Invert(bool, bool)")]
-        public static Vector2Int InvertVector2Int(this Vector2Int V, bool x = true, bool y = true)
-            => Invert(V, x, y);
-        [Obsolete("Use Vector2Int.InvertX()")]
-        public static Vector2Int InvertVector2IntX(this Vector2Int V)
-            => InvertX(V);
-        [Obsolete("Use Vector2Int.InvertY()")]
-        public static Vector2Int InvertVector2IntY(this Vector2Int V)
-            => InvertY(V);
-        [Obsolete("Use Vector2Int.Division(Vector2Int)")]
-        public static Vector2Int Divisor(this Vector2Int V, Vector2Int B)
-            => Division(V, B);
-        [Obsolete("Use Vector2Int.Multiplication(Vector2Int)")]
-        public static Vector2Int Multiplicador(this Vector2Int V, Vector2Int B)
-            => Multiplication(V, B);
-        [Obsolete("Usar converção explícita")]
-        public static Vector2Int ParaVector3(this Vector2Int V)
-            => V;
-        [Obsolete]
-        public static Vector2Int RemoveNaN(this Vector2Int V)
-            => new Vector2Int(
-                (V.x.ToString() == "NaN") ? 0 : V.x,
-                (V.y.ToString() == "NaN") ? 0 : V.y);
-        [Obsolete("Use Vector2Int.Summation()")]
-        public static float SomaDeTodosOsEixos(this Vector2Int V)
-            => V.x + V.y;
-        #endregion
     }
 }
